3f725a483923a6c654ac73d91c14df825d46bd03
fantom <ghostarafa@gmail.com>
2017-02-22 18:12:17 +0200

    stock values

--- worker.py
+++ worker.py
@@ -9,7 +9,7 @@ listen = ['default']
 # redis_url = os.getenv('REDISTOGO_URL', 'redis://localhost:6379')
 #
 # conn = redis.from_url(redis_url)
-redis_url = os.getenv('REDISTOGO_URL')
+redis_url = os.getenv('REDIS_URL')
 
 urlparse.uses_netloc.append('redis')
 url = urlparse.urlparse(redis_url)
