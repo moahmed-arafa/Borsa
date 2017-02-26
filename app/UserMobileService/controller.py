a407bcbe1d8d0c9d1eb5dbf82f614e4ff3ac7858
Master105 <ghotarafa@gmail.com>
2017-02-26 21:04:25 +0200

    dodo

--- app/UserMobileService/controller.py
+++ app/UserMobileService/controller.py
@@ -101,6 +101,7 @@ def get_company():
 def get_stock():
     if request.headers.get('Authorization') == API_KEY:
         req_json = request.get_json()
+        print(req_json)
         stock_id = req_json['stock_id']
         print(str(stock_id))
         stock = db.session.query(models.Stock).filter_by(id=stock_id).first()
